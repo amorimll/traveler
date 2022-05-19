a660e7d46707fcdb3015e2a0b43ca8cbe2cdd456
amorïm <63107417+amorimll@users.noreply.github.com>
2022-05-19 10:36:23 -0300

    Add files via upload

---
+++ Scripts/dim_loja.sql
@@ -0,0 +1,10 @@
+create table dim_viagem (
+	sk_loja INT PRIMARY KEY,
+	nk_loja INT,
+	cidade VARCHAR(14),
+	bairro VARCHAR(17)
+);
+insert into dim_viagem (sk_loja, nk_loja, cidade, bairro) values (1, 1, 'Belo Horizonte', 'Bonfim');
+insert into dim_viagem (sk_loja, nk_loja, cidade, bairro) values (2, 2, 'Rio de Janeiro', 'Copacabana');
+insert into dim_viagem (sk_loja, nk_loja, cidade, bairro) values (3, 3, 'Salvador', 'Alto da Terezina');
+insert into dim_viagem (sk_loja, nk_loja, cidade, bairro) values (4, 4, 'São Paulo', 'Jardim Paulista');
