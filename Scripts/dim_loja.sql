634dc881cc7324234fb1ab7835160ddbdcb838ff
amorïm <63107417+amorimll@users.noreply.github.com>
2022-05-22 18:21:35 -0300

    Update dim_loja.sql

--- Scripts/dim_loja.sql
+++ Scripts/dim_loja.sql
@@ -1,9 +1,9 @@
-create table dim_viagem (
+create table dim_loja (
 	cod_loja INT PRIMARY KEY,
 	cidade VARCHAR(14),
 	bairro VARCHAR(17)
 );
-insert into dim_viagem (cod_loja, cidade, bairro) values (1, 'Belo Horizonte', 'Bonfim');
-insert into dim_viagem (cod_loja, cidade, bairro) values (2, 'Rio de Janeiro', 'Copacabana');
-insert into dim_viagem (cod_loja, cidade, bairro) values (3, 'Salvador', 'Alto da Terezina');
-insert into dim_viagem (cod_loja, cidade, bairro) values (4, 'São Paulo', 'Jardim Paulista');
+insert into dim_loja (cod_loja, cidade, bairro) values (1, 'Belo Horizonte', 'Bonfim');
+insert into dim_loja(cod_loja, cidade, bairro) values (2, 'Rio de Janeiro', 'Copacabana');
+insert into dim_loja (cod_loja, cidade, bairro) values (3, 'Salvador', 'Alto da Terezina');
+insert into dim_loja (cod_loja, cidade, bairro) values (4, 'São Paulo', 'Jardim Paulista');
